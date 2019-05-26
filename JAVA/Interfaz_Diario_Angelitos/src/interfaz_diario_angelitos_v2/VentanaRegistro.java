ed5b2c60d10089cea5ea96a62638a3517fbd99d0
Luismiguel Ortiz <43765478+Hipsy-luu@users.noreply.github.com>
2019-05-26 11:29:42 -0600

    Interfaz para añadir el niño a la lista de asistencia
    aun no tiene como seleccionar el tutor que lo trajo y que le da la salida

--- JAVA/Interfaz_Diario_Angelitos/src/interfaz_diario_angelitos_v2/VentanaRegistro.java
+++ JAVA/Interfaz_Diario_Angelitos/src/interfaz_diario_angelitos_v2/VentanaRegistro.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package interfaz_diario_angelitos_v2;
 
 import algoritmosApoyo.TrieAutocompletar;
